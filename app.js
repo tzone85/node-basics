2ee625d930050e832a44bc57dba8eda8a6006fc6
thando mini <tzone85@gmail.com>
2019-06-15 20:25:19 +0200

    making use of async node functions to read a file and write the contents to a new file

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
-var events = require('events');
-var util = require('util');
 var fs = require('fs');
-
-// handling events and emitters.
-
-/*
-var myEmitter = new events.EventEmitter();
-
-myEmitter.on('someEvent', function (mssg) {
-    console.log(mssg);
-});
-
-myEmitter.emit('someEvent', 'the event was emitted');
-*/
-
-/*
-var Person = function (name) {
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-
-var people = [james, mary, ryu];
-
-people.forEach(function (person) {
-   person.on('speak', function (mssg) {
-      console.log(person.name + ' said: ' + mssg);
-   });
-});
-
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', 'Ndingumqwathi');
-*/
 
 // Reading files
 
 var readme = fs.readFileSync('readme.txt', 'utf8');
 
-console.log(readme);
-
+fs.writeFileSync('writeMe.txt', readme);
