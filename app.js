e1d65706465cb2a373e1134d1cb9739b07815945
thando mini <tzone85@gmail.com>
2019-06-16 15:13:22 +0200

    using the pipe method ot read from readable stream and write to a writable stream

--- app.js
+++ app.js
@@ -4,7 +4,5 @@ var fs = require('fs');
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
 var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
 
-myReadStream.on('data', function (chunk) {
-   console.log('new chunk received: ');
-    myWriteStream.write(chunk);
-});
+// pipe from a readable stream to a writable stream
+myReadStream.pipe(myWriteStream);
