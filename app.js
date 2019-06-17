b5c2c1c4cc0040fde80bdafe8ef3248b01810e54
thando mini <tzone85@gmail.com>
2019-06-17 14:55:34 +0200

    added basic routing to my project

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ var fs = require('fs');
 
 
 var server = http.createServer(function (req, res) {
-    console.log('request was made' + req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var myObj = {
-        name: 'Rhyu',
-        job: 'Ninja',
-        age: 29
-    };
-    res.end(JSON.stringify(myObj));
+    console.log('request was made: ' + req.url);
 
+    if (req.url === '/home' || req.url === '/') {
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact') {
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if (req.url === '/api/ninjas') {
+
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 30}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 server.listen(3000, '127.0.0.1');
