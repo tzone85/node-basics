a366a26e9cccb9844c525d8a05a848c1ee31a721
thando mini <tzone85@gmail.com>
2019-06-17 22:42:42 +0200

    added dynamic data and displayed it to the views by making use of ejs templating engine

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-   res.send('this is the homepage');
+   res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, res) {
-    res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 // route variables using wrap parameters
 
-app.get('/profile/:id', function (req, res) {
-   res.send('You requested to see a profile with the id of ' + req.params.id);
+app.get('/profile/:name', function (req, res) {
+    var data = {age: 29, job: 'ninja'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
