386689bafbf1eba6a8fbdc9c8f2925c8b727f1c2
thando mini <tzone85@gmail.com>
2019-06-19 00:12:00 +0200

    added a small form and utalized query strings to prepopulate data into the form fields

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('stuff'));
 
 app.get('/', function (req, res) {
    res.render('index');
 });
 
 app.get('/contact', function (req, res) {
-    res.render('contact');
+    console.log(req.query);
+    res.render('contact', { querryString: req.query });
 });
 
 // route variables using wrap parameters
