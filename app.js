d1b2ff94fd7b6e2ea8d7b61f98a8f8a58f7137fc
tzone85 <tzone85@gmail.com>
2019-06-19 13:02:23 +0200

    created a controller, then required the controller in our entry point, viz app.js then made the routes within the controller

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var todoControler = require('./controllers/todoController');
 
 var app = express();
 
@@ -9,7 +9,9 @@ app.set('view engine', 'ejs');
 // static files (by removing the first parameter of the 'use' middleware, it's not going to be route specific)
 app.use(express.static('/public'));
 
-// listen to port
+// fire controller
+todoControler(app);
 
+// listen to port
 app.listen(3000);
 console.log('Listening to port: 3000');
