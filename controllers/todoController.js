42dde4152864a0f83ab2f49404781c834fd024f8
tzone85 <tzone85@gmail.com>
2019-06-23 15:16:20 +0200

    added to the todo list dynamically. But I have a bug that for it adds it in json format but works and I need to do something else

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,20 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'Get Food'}, {item: 'Walk Dog'}, {item: 'Kick some coding ass'}];
+
+// bodyParser.urlencoded is the middleware to be run on the post request
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
 module.exports = function (app) {
 
     app.get('/todo', function (req, res) {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function (req, res) {
-
+    app.post('/todo', urlencodedParser, function (req, res) {
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo', function (req, res) {
